fe2bcbef40b27553c0734bd7b4506712b944bb45
JaimeCarcamoSepulveda <jaimecarcamobroker@gmail.com>
2022-10-30 18:43:22 -0700

    Remove bugs on plane animation and improved response of weather display
    Put a time on reset of plane animation so that it can reset for next
    weather query. Added CSS classes to weather-response script to make
    weather forecast display horizontally. Removed bugs for city, state and
    country. Synchronized weather-dashboard opacity to plane animation.

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ finally create elements in DOM to add HTML for weather results.
 
 
 const form = document.querySelector(`#form`);
-form.addEventListener('submit', getLocation)
+form.addEventListener('submit', getCoordinates);
+form.addEventListener('submit', flyAway);
 
 //const cityName = document.getElementById("city-name");
 //const stateName = document.getElementById("state-name");
@@ -27,19 +28,120 @@ let countrySelected = ''
 let unitType = ''
 let URLWeather = ''
 
+/* -------API WEATHER CALLS -----------------------------------------------------------------*/
+const OpenWeatherMapAPIkey = '87777ca4cdeac88a2810d012d3dff954'
+/* via template literals completes needed fields for API query needed to get coordinates-- city, state(ony for USA ), countryISO */
+function getCoordinates (e) {
+    e.preventDefault();
+    const city = (this.querySelector('[id=city-name]')).value;
+    const state = (this.querySelector('[id=state-name]')).value;
+    let unit = (this.querySelector('[id=unit-name]')).value;
+    let getUnitType = (unit) => unit == 'fahrenheit'?'imperial': 'metric';
+    unitType = getUnitType(unit);
+    cityEntered = city.toUpperCase();
+    stateEntered = state.toUpperCase();
+    URLCoords = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${isoCountryCode}&limit=1&appid=${OpenWeatherMapAPIkey}`; 
+    getLocation(URLCoords)
+
+    this.reset(); 
+}
+/* Makes an API request with city, state and isoCountryCode returns latitude and longitude coordinates */
+const getLocation = async (URLCoords) => {
+   const response = await fetch(URLCoords);
+   const coordinates = await response.json(); 
+   const { 0: { lat, lon }  } = coordinates;
+   URLWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OpenWeatherMapAPIkey}&units=${unitType}`
+    timesToRun(3);
+};
+
+//fetch weather forecast. Make API weather call
+let weatherResults = {};
+const getWeather = async () => {
+    const weatherRes = await fetch(URLWeather);
+    const weather = await weatherRes.json();
+    weatherResults =weather;
+    inputWeatherResult();   
+};
+
+
+const weatherDashboard = document.querySelector('.weather-dashboard');
+//dynamically inserts weather forecast results into webpage
+function inputWeatherResult () {
+    const {  main: { temp, humidity,temp_min, temp_max, pressure },
+            wind: { speed }, clouds: { all: cloudinessPercent }, weather: { 0: { description: weatherDesc } } } = weatherResults;
+            const div = document.createElement('div');
+            div.className = "html-container"
+            let country = countrySelected.toUpperCase();
+            if (stateEntered == '') { stateEntered = `${country}`; country = '';}
+            else{ country = `, ${country}`; };
+            
+            let html = `
+            <h3 class= "city-state">${cityEntered}, ${stateEntered} ${country} </h3>
+            <div class="weather-results">
+                <div >
+                <p class = "response-text">current temperature is ${temp}</p> 
+                <p class = "response-text">minimum temperature ${temp_min}</p>
+                <p class = "response-text">maximum temperature ${temp_max}</p> 
+                </div>
+                <div>
+                <p class = "response-text">humidity ${humidity}</p>
+                <p class = "response-text">SUNRISE AT</p>
+                <p class = "response-text">SUNSET AT</p>
+                </div>
+                <div>
+                <p class = "response-text">Pressure is at ${pressure}</p>
+                <p class = "response-text">weather description: ${weatherDesc}</p>
+                <p class = "response-text">cloudiness is at ${cloudinessPercent}%</p>
+                <p class = "response-text">wind speeds are at ${speed} mph</p>
+                </div>
+            </div>
+            `
+            div.innerHTML = html
+            weatherDashboard.append(div)
+        }
+
+
+//adds fly-away class to #plane-flying container so plane animation can initiate then removes class 
+//setting up for next search query
+const planeFlying = document.querySelector('#plane-flying');
+function flyAway() {
+    planeFlying.setAttribute('class', 'fly-away');
+    weatherDashboard.setAttribute('style', 'opacity: 0;');
+    setTimeout( () => { 
+        planeFlying.removeAttribute('class', 'fly-away');
+        weatherDashboard.setAttribute('style', 'opacity: 1;');
+}, "2200")
+    
+}
+
+
+
+//---------PERIPHERAL FUNCTIONS BELOW ----------------------------------------------------------------------------------
+  
+//limits weather query amounts by number of queryTimes entered -if query times not exceeded the it calls getWeather()
+let queryCount = 0;
+function timesToRun(queryTimes) {
+    if(queryCount < queryTimes) {
+     queryCount++;
+     getWeather();
+    } else { alert(`We apologize, only a limited amount of weather queries are allowed`) }
+ }
+ 
 
 
 /*
-1st - Will auto populate country input if 1 match remains
+1st - Will auto-populate country input if 1 match remains
 2nd- Will allow for selection of items in autocomplete list to target, click and populate into country input
 3rd.- Once a country is selected will grab the matching code and insert into fetch call 
 */
-
+const searchInput = document.getElementById("country-name");
 searchInput.addEventListener('input', displayCountries);
 searchInput.addEventListener('keyup', displayCountries);
 
-/* ------------ gets country obj array from local fetch, auto-completes and selects isoCode for country ------------------------------ */
-const searchInput = document.getElementById("country-name");
+/* --------get country obj array from local fetch, 
+auto-completes and
+ selects isoCode for country ------------------------------ */
+
 const suggestions = document.querySelector('.suggestions');
 //----gets json file and displays when typing 
 const countries = [ ];
@@ -100,81 +202,3 @@ countryField.addEventListener('keydown', function(e) {
         countryField.append(htmlCountry);
     }
 });
-
-/* -------API WEATHER CALLS -----------------------------------------------------------------*/
-const OpenWeatherMapAPIkey = '87777ca4cdeac88a2810d012d3dff954'
-/* via template literals completes needed fields for API query needed to get coordinates-- city, state(ony for USA ), countryISO */
-function getCoordinates (e) {
-    e.preventDefault();
-    const city = (this.querySelector('[id=city-name]')).value;
-    const state = (this.querySelector('[id=state-name]')).value;
-    let unit = (this.querySelector('[id=unit-name]')).value;
-    let getUnitType = (unit) => unit == 'fahrenheit'?'imperial': 'metric';
-    unitType = getUnitType(unit);
-    cityEntered = city.toUpperCase();
-    stateEntered = state.toUpperCase();
-    URLCoords = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${isoCountryCode}&limit=1&appid=${OpenWeatherMapAPIkey}`; 
-    getLocation(URLCoords)
-    this.reset(); 
-}
-/* Makes an API request with city, state and isoCountryCode returns latitude and longitude coordinates */
-const getLocation = async (URLCoords) => {
-   const response = await fetch(URLCoords);
-   const coordinates = await response.json(); 
-   const { 0: { lat, lon, country = 'US'}  } = coordinates;
-   URLWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OpenWeatherMapAPIkey}&units=${unitType}`
-    timesToRun(3);
-};
-
-//fetch weather forecast. Make API weather call
-let weatherResults = {};
-const getWeather = async () => {
-    const weatherRes = await fetch(URLWeather);
-    const weather = await weatherRes.json();
-    weatherResults =weather;
-    inputWeatherResult();   
-};
-
-//dynamically inserts weather forecast results into webpage
-function inputWeatherResult () {
-    const {  main: { temp, humidity,temp_min, temp_max, pressure },
-            wind: { speed }, clouds: { all: cloudinessPercent }, weather: { 0: { description: weatherDesc } } } = weatherResults;
-            const div = document.createElement('div');
-            div.className = "html-container"
-            const contain = document.querySelector('.weather-response');
-            
-            let html = `
-            <h3 class= "city-state">${cityEntered}, ${stateEntered} </h3>
-            <div >
-            <p class = "response-text">current temperature is ${temp}</p> 
-            <p class = "response-text">minimum temperature ${temp_min}</p>
-            <p class = "response-text">maximum temperature ${temp_max}</p> 
-            </div>
-            <div>
-            <p class = "response-text">humidity ${humidity}</p>
-            <p class = "response-text">SUNRISE AT</p>
-            <p class = "response-text">SUNSET AT</p>
-            </div>
-            <div>
-            <p class = "response-text">Pressure is at ${pressure}</p>
-            <p class = "response-text">weather description: ${weatherDesc}</p>
-            <p class = "response-text">cloudiness is at ${cloudinessPercent}%</p>
-            <p class = "response-text">wind speeds are at ${speed} mph</p>
-            </div>
-            `
-            div.innerHTML = html
-            contain.append(div)
-        }
-
-
-//---------PERIPHERAL FUNCTIONS BELOW -------------------------------------------------------------------------------------------------
-  
-//limits weather query amounts by number of queryTimes entered -if query times not exceeded the it calls getWeather()
-let queryCount = 0;
-function timesToRun(queryTimes) {
-    if(queryCount < queryTimes) {
-     queryCount++;
-     getWeather();
-    } else { alert(`We apologize, only a limited amount of weather queries are allowed`) }
- }
- 
